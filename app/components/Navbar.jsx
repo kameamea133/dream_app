37d8ac5d9045d59b5f3248ac3fbf4aa4013ff6c5
kameamea133 <monnier1977@gmail.com>
2024-08-29 08:33:06 +0200

    Navbar component created

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center fixed w-full px-[150px]'>
+        <div>
+            <Image 
+                src='/assets/images/Image 2.png'
+                width={100}
+                height={100}
+                alt='logo'
+                className='w-[120px] h-[120px]'
+            />
+        </div>
+        <div className='pb-5'>
+        <RxHamburgerMenu size={30}/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
